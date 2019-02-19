de1f537c24dde2c83108ed382b79a1c24a2ddef1
Hữu Minh <huuminh2101@gmail.com>
2019-02-19 23:05:27 +0700

    chưa ra kết quả
    bị âm hoặc ko có kết quả

--- main.c
+++ main.c
@@ -12,7 +12,12 @@
  * 
  */
 int main(int argc, char** argv) {
-
+int r ;
+    printf("Nhập bán kính đường tròn r : "); scanf("%d", &r);
+     
+    printf("\n Chu vi hình tròn có bán kính =%d",3.141592*2*r);
+    printf("\n Diện tích hình  tròn có bán kính =%d \n",3.141592*r*r);
+    
     return (EXIT_SUCCESS);
 }
 
