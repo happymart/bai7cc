baf0084bc42b70a809cbe89cf904c7973d3aede9
Hữu Minh <huuminh2101@gmail.com>
2019-02-19 22:21:26 +0700

    ok

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Administrator
+ *
+ * Created on February 19, 2019, 10:15 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
